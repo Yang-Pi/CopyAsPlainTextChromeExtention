80825cd6f8fd3f67c28127a6aa8f9cf9defb48ae
yaroslav.pylaev <56zeBVPax8kPgjM>
2022-02-08 15:26:39 +0300

    move extention to mv3

--- src/oncopy.js
+++ src/oncopy.js
@@ -1,6 +1,20 @@
 document.addEventListener('copy', () => {
-    chrome.runtime?.sendMessage({
-        command: 'copy', 
-        textContent: window.getSelection()?.toString()
-    });
+    const textArea = document.createElement('textarea');
+    textArea.value = window.getSelection()?.toString()?.trim();
+
+    textArea.style.top = '0';
+    textArea.style.left = '0';
+    textArea.style.position = 'fixed';
+
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+
+    try {
+        document.execCommand('cut');
+    } catch (err) {
+        console.error('Keyboard buffer: Oops, unable to write plain text', err);
+    }
+
+    document.body.removeChild(textArea);
 });
