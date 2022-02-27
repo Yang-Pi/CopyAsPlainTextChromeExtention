2ddfa3c29a96a7a4c728291b47985810df9781f3
yaroslav.pylaev <56zeBVPax8kPgjM>
2022-02-27 21:23:21 +0300

    fix missing images in copy result: now link insted of image

--- src/oncopy.js
+++ src/oncopy.js
@@ -1,10 +1,34 @@
 document.addEventListener('copy', async (e) => {
     e.preventDefault();
     try {
-        const plaintText = window.getSelection()?.toString()?.trim();
-        if (plaintText) {
-            await navigator.clipboard.writeText(plaintText);
+        let plainText = '';
+
+        var range = window.getSelection().getRangeAt(0);
+        var fragment = range.cloneContents();
+
+        for (let i = 0; i < fragment.children.length; i++) {
+
+          const textContent = fragment.children[i].textContent.trim();
+          if (textContent !== '') {
+            plainText
+              += (plainText === '' ? '' : '\n')
+                + textContent;
+            continue;
+          }
+
+          const imageContent = fragment.querySelector('img');
+          if (imageContent) {
+            plainText
+              += (plainText === '' ? '' : '\n')
+                + imageContent.currentSrc
+                + '\n';
+          }
         }
+
+        if (plainText) {
+            await navigator.clipboard.writeText(plainText);
+        }
+
     } catch (err) {
       console.error(err.name, err.message);
     }
